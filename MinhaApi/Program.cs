7f7bcfab723eddf593af669d423771980f64564e
viniciusgmacedo <macedovinicius2004@gmail.com>
2025-12-14 22:22:23 -0300

    feat/get-created

--- MinhaApi/Program.cs
+++ MinhaApi/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<applicationDBContext>(options =>
 {
@@ -40,6 +41,8 @@ app.UseHttpsRedirection();
 //     return forecast;
 // })
 // .WithName("GetWeatherForecast");
+
+app.MapControllers();
 
 app.Run();
 
