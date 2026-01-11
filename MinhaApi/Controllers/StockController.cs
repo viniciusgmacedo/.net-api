1487c273bd51649f4e08de16a9e579b886da6486
viniciusgmacedo <macedovinicius2004@gmail.com>
2026-01-11 12:07:44 -0300

    fix:controller now uses async and await for operations

--- MinhaApi/Controllers/StockController.cs
+++ MinhaApi/Controllers/StockController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using MinhaApi.data;
 using MinhaApi.Mappers.StockMappers;
@@ -22,16 +23,17 @@ namespace MinhaApi.Controllers
         }
 
         [HttpGet] //rota para pegar todos os stocks
-        public IActionResult GetAll() //pega todos os stocks
+        public async Task<IActionResult> GetAll() //pega todos os stocks
         {
-            var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto()); // usa o tolist para converter em lista e pegar todos os stocks
-            return Ok(stocks); //retorna a lista de stocks com status 200 OK
+            var stocks = await _context.Stocks.ToListAsync();
+            var stockDTO = stocks.Select(s => s.ToStockDto()); // usa o tolist para converter em lista e pegar todos os stocks
+            return Ok(stockDTO); //retorna a lista de stocks com status 200 OK
         }
 
         [HttpGet("{id}")] //rota para pegar um stock pelo id
-        public IActionResult GetById([FromRoute] int id) //fromroute para pegar o id da rota
+        public async Task<IActionResult> GetById([FromRoute] int id) //fromroute para pegar o id da rota
         {
-            var stock = _context.Stocks.Find(id); //usa o find para procurar o stock pelo id
+            var stock = await _context.Stocks.FindAsync(id); //usa o find para procurar o stock pelo id
             if (stock == null)
             {
                 return NotFound();
@@ -41,11 +43,11 @@ namespace MinhaApi.Controllers
 
         [HttpPost] //rota para criar um novo stock
 
-        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
             var stockModel = stockDto.ToStockFromCreateDTO(); //converte o CreateStockRequestDto para Stock
-            _context.Stocks.Add(stockModel); //adiciona o novo stock ao contexto
-            _context.SaveChanges(); //salva as mudanças no banco de dados
+            await _context.Stocks.AddAsync(stockModel); //adiciona o novo stock ao contexto
+            await _context.SaveChangesAsync(); //salva as mudanças no banco de dados
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto()); //retorna o stock criado com status 201 Created
 
         }
@@ -53,9 +55,9 @@ namespace MinhaApi.Controllers
         [HttpPatch] //rota para criar um novo stock
         [Route("{id}")]
 
-        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateStockDto) //aqii tenho o id e o corpo da requisição
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateStockDto) //aqii tenho o id e o corpo da requisição
         {
-            var stockModel = _context.Stocks.Find(id); //procura o stock pelo id
+            var stockModel = await _context.Stocks.FindAsync(id); //procura o stock pelo id
             if (stockModel == null)
             {
                 return NotFound(); //se não encontrar, retorna 404 Not Found
@@ -67,22 +69,22 @@ namespace MinhaApi.Controllers
             if (updateStockDto.Industry is not null) stockModel.Industry = updateStockDto.Industry; //atualiza a indústria se fornecido
             if (updateStockDto.MarketCap.HasValue) stockModel.MarketCap = updateStockDto.MarketCap.Value; //atualiza o valor de mercado se fornecido
 
-            _context.SaveChanges(); //salva as mudanças no banco de dados
+            await _context.SaveChangesAsync(); //salva as mudanças no banco de dados
             return Ok(stockModel.ToStockDto()); //retorna o stock atualizado com status 200 OK
 
         }
 
         [HttpDelete]
         [Route("{id}")]
-        public IActionResult Delete([FromRoute] int id) //deleta um stock pelo id
+        public async Task<IActionResult> Delete([FromRoute] int id) //deleta um stock pelo id
         {
-            var stockModel = _context.Stocks.FirstOrDefault(s => s.Id == id); //procura o stock pelo id
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(s => s.Id == id); //procura o stock pelo id
             if (stockModel == null)
             {
                 return NotFound(); //se não encontrar, retorna 404 Not Found
             }
             _context.Stocks.Remove(stockModel); //remove o stock do contexto
-            _context.SaveChanges(); //salva as mudanças no banco de dados
+            await _context.SaveChangesAsync(); //salva as mudanças no banco de dados
             return NoContent(); //retorna 204 No Content
         }
 
